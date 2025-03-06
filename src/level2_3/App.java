3e77c633ddfbb36b020242dd20990e3ce96898d3
최성원 <90265239+S-Woni@users.noreply.github.com>
2025-03-06 13:14:12 +0900

    Feat: "Lv 2-3. 계산기 표현식 평가 구현"
    한번에 숫자와 연산식을 여러개 넣고 계산하는 기능 구현

--- src/level2_3/App.java
+++ src/level2_3/App.java
@@ -1,27 +1,24 @@
 package level2_3;
 
-import java.util.List;
 import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
-
         Calculator calculator = new Calculator();
 
         System.out.println();
         System.out.println("계산기를 시작합니다");
 
-        while(true) {
+        while (true) {
             System.out.println();
             System.out.println("-------------(exit 입력 시 종료)---------------");
 
-            List<Integer> numbers = calculator.getValidNumber();
-            char operator = calculator.getValidOperator();
-
-            calculator.printPerformCalculation(numbers, operator);
-
+            String expression = calculator.getValidExpression();
+            int result = calculator.evaluateExpression(expression);
+            calculator.setResults(result);
+            calculator.printResults(result, expression);
         }
     }
 }
