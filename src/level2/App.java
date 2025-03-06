25084591762a488575d0b7ea707c5b1c5cf9f7a2
최성원 <90265239+S-Woni@users.noreply.github.com>
2025-03-06 13:43:16 +0900

    Comment: "Lv 2 계산기 주석 추가"

--- src/level2/App.java
+++ src/level2/App.java
@@ -15,9 +15,14 @@ public class App {
         while(true) {
             System.out.println();
             System.out.println("-------------(exit 입력 시 종료)---------------");
-
+            
+            // 첫 번째 숫자를 입력 받고 유효한 숫자일 경우 firstNum에 저장
             int firstNum = calculator.getValidNumber(1);
+
+            // 첫 번째 숫자를 입력 받고 유효한 숫자일 경우 secondNum에 저장
             int secondNum = calculator.getValidNumber(2);
+            
+            // 연산자를 입력 받고 연산자가 유효하면 값을 반환하여 받아옴
             char operator = calculator.getValidOperator(secondNum);
 
             calculator.printPerformCalculation(firstNum, secondNum, operator);
--- src/level2/Calculator.java
+++ src/level2/Calculator.java
@@ -14,10 +14,12 @@ public class Calculator {
         this.results = new LinkedList<>();
     }
 
+    // 계산 결과를 results에 저장
     public void setResults(int result) {
         results.offer(result);
     }
 
+    // 계산 결과를 하나 삭제
     public void removeResult() {
         if (!results.isEmpty()) {
             results.poll();
@@ -26,14 +28,17 @@ public class Calculator {
         }
     }
 
+    // 저장된 계산 결과를 반환
     public Queue<Integer> getResults() {
         return results;
     }
 
+    // 유요한 숫자를 입력받아 반환
     public int getValidNumber(int tempNumber) {
         while (true) {
             String tempString;
 
+            // 첫 번째 숫자와 두 번째 숫자를 구분하기 위한 문자열
             if (tempNumber == 1) {
                 tempString = "첫";
             } else {
@@ -43,6 +48,7 @@ public class Calculator {
             System.out.print(tempString + " 번째 숫자를 입력해주세요 : ");
 
             try {
+                // exit 입력시 계산기 종료
                 if (scanner.hasNext("exit")) {
                     System.out.println();
                     System.out.println("계산기를 종료합니다.");
@@ -52,6 +58,7 @@ public class Calculator {
 
                 int number = scanner.nextInt();
 
+                // 음수인 경우 예외 처리
                 if (number < 0) {
                     throw new IllegalArgumentException("\n음수를 입력하셨습니다.\n양수를 입력해주세요.\n");
                 }
@@ -69,6 +76,7 @@ public class Calculator {
         }
     }
 
+    // 유효한 연산자를 입력 받아 반환
     public char getValidOperator(int secondNum) {
         while(true) {
             System.out.print("사칙 연산 기호를 입력하세요 (+, -, *, /) : ");
@@ -81,11 +89,14 @@ public class Calculator {
                     System.exit(0);
                 }
 
+                // charAt(0)을 통해 맨 앞 문자만 가져옴
                 char operators = scanner.next().charAt(0);
 
+                // + - * / 중 하나가 아닐시 예외 처리
                 if ("+-*/".indexOf(operators) == -1) {
                     throw new IllegalArgumentException("\n잘못된 입력입니다.\n+, -, *, / 중 하나를 입력해주세요.\n");
                 }
+                // 0으로 나누려고 하면 예외처리
                 if (secondNum == 0 && operators == '/') {
                     throw new ArithmeticException("\n나눗셈 연산에서 두번째 정수에 0이 입력될 수 없습니다.\n다시 입력해주세요.\n");
                 }
@@ -97,6 +108,7 @@ public class Calculator {
         }
     }
 
+    // 계산을 수행 및 결과 출력
     public void printPerformCalculation(int firstNum, int secondNum, char operator) {
         int result = switch (operator) {
             case '+' -> firstNum + secondNum;
@@ -109,21 +121,26 @@ public class Calculator {
         System.out.println();
         System.out.println("----------------------------------------");
 
+        // 결과 저장
         setResults(result);
+        // 저장된 결과 출력
         System.out.print("저장된 계산 결과 : ");
         for (int i : getResults()) {
             System.out.print(i + " ");
         }
 
+        // 결과 출력
         System.out.println();
         System.out.println("결과 : " + firstNum + " " + operator + " " + secondNum + " = " + result);
         System.out.println();
 
         System.out.println("------------(del 입력 시 삭제)------------");
         System.out.print("저장된 결과를 삭제하시겠습니까? : ");
+        // del 입력시 결과 하나 삭제
         if (scanner.hasNext("del")) {
             System.out.println();
 
+            // 결과 삭제
             removeResult();
 
             System.out.print("저장된 계산 결과 : ");
